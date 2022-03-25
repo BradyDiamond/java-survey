f5e5675dccb9fdc4e9dc786b3d0d43a774ec6c0c
Brady Diamond <brdydmnd@gmail.com>
2022-03-25 11:52:33 -0700

    wip lunch

--- js/scripts.js
+++ js/scripts.js
@@ -8,40 +8,21 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
-    const name = $("#name1").val();
-    console.log ("name");
-    const city = parseInt($("#city").val());
-    const time = parseInt($("#time").val());
-    const pace = parseInt($("#pace").val());
-    const work = parseInt($("#work").val());
-    const sum = (city + time + work + pace)
+    // const name = $("#name1").val();
+    // console.log ("name");
+    let city = parseInt($("#city").val());
+    // const time = parseInt($("#time").val());
+    // const pace = parseInt($("#pace").val());
+    // const work = parseInt($("#work").val());
+    // const sum = (city + time + work + pace)
   
-    console.log ("number");
-    if (sum > 5) {
+    if (city <= 2) {
     // if (city + time + work + pace > 5) {
       $("#rust").show();
     // } else if (city + time + work + pace > 5) {
     // $("#Ruby").show();
-    // } else if (city + time + work + pace > 5) {
-    // $("#python").show(); 
+    } else if  (city <= 2) {
+    $("#python").show(); 
     }
   });
 });
-
-
-
-
-
-
-
-
-
-
-// if (age > 21) {
-//   $('#drinks').show();
-// } else if (age === 21) {
-//   alert("Now don't go wild!");
-//   $('#drinks').show();
-// } else {
-//   $('#under-21').show();
-// }
